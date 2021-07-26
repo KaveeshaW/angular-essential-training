102e15063773399f5b3d3f2659e9ac81211c8e96
KaveeshaW <weerasirikaveesha@gmail.com>
2021-07-26 16:45:57 -0500

    changed the css, updated the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from "@angular/core"; 
 
 @Component({
-    selector: 'app-root',
-    template: '<h1>My App</h1>'
+    selector: 'mw-app', // media watcher
+    // template means you have the html inline, instead of templateUrl where the html is somewhere else
+    templateUrl: './app.component.html', // inline within the component metadata
+    styleUrls: ['./app.component.css'] // scope css, don't need to do too much for building stuff
 })
 export class AppComponent {}
