80f65735fc270e931e7d3fc03e948d1c8841d481
KaveeshaW <weerasirikaveesha@gmail.com>
2021-07-26 17:14:42 -0500

    media-item-component is added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from './app.component';
+import { MediaItemComponent } from "./media-item.component";
 // metadata
 @NgModule({
     imports: [
         BrowserModule
     ], // get everything you need
     declarations: [
-        AppComponent
+        AppComponent,
+        MediaItemComponent
     ], // used to make components
     bootstrap: [
         AppComponent
